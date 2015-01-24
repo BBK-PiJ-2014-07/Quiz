be2e874e4e1c92534b5fdbda1f3b2edc5c2878e3
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-01-24 14:33:15 +0000

    Wrote fields and constructor for Quiz class.

--- src/quiz/Quiz.java
+++ src/quiz/Quiz.java
@@ -7,4 +7,18 @@ import java.io.Serializable;
  * @author Sophie Koonin
  */
 public class Quiz implements Serializable {
+    private int id;
+    //static int to ensure unique id for each quiz
+    private static int questionIds = 0;
+    private static final int MAX_QUESTIONS = 4;
+    private Question[] questions;
+
+    public Quiz(){
+        //instantiate array of questions - max number is 4
+        questions = new Question[MAX_QUESTIONS];
+        id = ++questionIds;
+    }
+    protected class Question {
+
+    }
 }
