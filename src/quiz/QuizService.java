1239b2736888777367144edc9902287fe819aaa8
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-01-24 14:02:33 +0000

    Adding initial outline of methods to server interface.

--- src/quiz/QuizService.java
+++ src/quiz/QuizService.java
@@ -1,8 +1,27 @@
 package quiz;
 
+import java.rmi.Remote;
+
 /**
  * Interface for Quiz Server.
  * @author Sophie Koonin
  */
-public interface QuizService {
+public interface QuizService extends Remote {
+    /**
+     * Play a selected quiz
+     * @param quizId - the id of the quiz to play
+     */
+    void playQuiz(int quizId);
+
+    /**
+     * Create a new quiz
+     * @return id of new quiz
+     */
+    int createQuiz();
+
+    /**
+     * Close the selected quiz
+     * @param quizId - the id of the quiz to close
+     */
+    void closeQuiz(int quizId);
 }
