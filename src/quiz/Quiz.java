5db931d987ebd1e55b30af5721a305f885158160
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-01-24 14:09:09 +0000

    Interface methods getId(), getQuestionList(), addQuestion(), play()

--- src/quiz/Quiz.java
+++ src/quiz/Quiz.java
@@ -5,5 +5,24 @@ package quiz;
  * @author Sophie Koonin
  */
 public interface Quiz {
+    /**
+     * @return the id of this quiz
+     */
+    int getId();
 
+    /**
+     * Get the list of questions
+     * @return the list of questions
+     */
+    ArrayList<Question> getQuestionList();
+
+    /**
+     * Add a new question to the quiz
+     */
+    void addQuestion();
+
+    /**
+     * Play the quiz
+     */
+    void play();
 }
