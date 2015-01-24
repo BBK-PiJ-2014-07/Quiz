03c2a7cb89981a39a51c8c81a9335878a619aa79
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-01-24 14:25:17 +0000

    create Quiz class

---
+++ src/quiz/Quiz.java
@@ -0,0 +1,10 @@
+package quiz;
+
+import java.io.Serializable;
+
+/**
+ * The quiz itself. Contains inner class Question.
+ * @author Sophie Koonin
+ */
+public class Quiz implements Serializable {
+}
