f02623a316806e5a1f5998d64261be676a70ce41
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-01-24 14:14:20 +0000

    Defined two methods for Question interface, isCorrect() and getAnswer()

--- src/quiz/Question.java
+++ src/quiz/Question.java
@@ -5,4 +5,17 @@ package quiz;
  * @author Sophie Koonin
  */
 public interface Question {
+    /**
+     * Checks player answer against value of correct answer and returns corresponding boolean
+     * @return true or false (if answer correct)
+     */
+    boolean isCorrect(String ans);
+
+    /**
+     *
+     * @return The correct answer to the question
+     */
+    String getAnswer();
+
+
 }
