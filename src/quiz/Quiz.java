57f4b49d68e75ea27077c1d101bca5e90fdc797f
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-01-24 14:42:46 +0000

    Confused Quiz for Question. Reassigning fields of Quiz - now contains an ArrayList of questions of undefined size. Renamed questionIds to quizIds.

--- src/quiz/Quiz.java
+++ src/quiz/Quiz.java
@@ -1,6 +1,7 @@
 package quiz;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * The quiz itself. Contains inner class Question.
@@ -9,25 +10,27 @@ import java.io.Serializable;
 public class Quiz implements Serializable {
     private int id;
     //static int to ensure unique id for each quiz
-    private static int questionIds = 0;
-    private static final int MAX_QUESTIONS = 4;
-    private Question[] questions;
+    private static int quizIds = 0;
+    private ArrayList<Question> questions;
 
     public Quiz(){
-        //instantiate array of questions - max number is 4
-        questions = new Question[MAX_QUESTIONS];
+        questions = new ArrayList<Question>;
         //pre-increment id number to assign as id of this quiz
-        id = ++questionIds;
+        id = ++quizIds;
     }
     protected class Question {
         private int questionNumber;
         private String question;
-        private String answer;
+        private String[] answer;
 
         private Question(int questionNumber, String question, String answer){
             this.questionNumber = questionNumber;
             this.question = question;
             this.answer = answer;
+        }
+
+        protected String getQuestion(){
+            return question;
         }
     }
 }
