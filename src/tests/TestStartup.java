e7a85cb3376722431161365a875f44b113d16418
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-02 12:29:36 +0000

    Instantiate testAddQuiz; set System.in to FileInputStream for testing purposes

--- src/tests/TestStartup.java
+++ src/tests/TestStartup.java
@@ -3,9 +3,25 @@ package tests;
 import org.junit.*;
 import static org.junit.Assert.*;
 import quiz.*;
+
+import java.io.*;
+
 /**
  * Created by Sophie on 02/02/15.
  * Test the start-up client
  */
 public class TestStartup {
+    Setup setup;
+
+    @Before
+    public void buildUp() throws FileNotFoundException {
+        setup = new Setup();
+        System.setIn(new FileInputStream("tests/testStartup.txt"));
+    }
+
+    @Test
+    public void testAddQuiz(){
+        setup.addQuiz("test quiz");
+    }
+
 }
