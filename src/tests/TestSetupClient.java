82e71ca1261b12d5f71d5e86e77f4a74f94e6754
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-02 15:16:02 +0000

    Moving functionality of createQuiz to SetupClient. Write test for createQuiz in TestSetupClient

--- src/tests/TestSetupClient.java
+++ src/tests/TestSetupClient.java
@@ -1,9 +1,11 @@
 package tests;
 
 import org.junit.*;
+import static org.junit.Assert.*;
 import quiz.*;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Created by Sophie on 02/02/15.
@@ -11,18 +13,22 @@ import java.io.*;
  */
 public class TestSetupClient {
     SetupClient setup;
+    QuizServer server;
 
     @Before
     public void buildUp() throws FileNotFoundException {
         setup = new SetupClient();
+        server = new QuizServer();
         System.setIn(new FileInputStream("tests/testStartup.txt"));
     }
 
     @Test
     public void testCreateQuiz(){
-        setup.createQuiz();
-        assertEquals("test quiz", setup.getQuizList.get(0).getQuizName);
-
+        String input = "test quiz\nwhat is the capital of england?\nlondon\nparis\nbeijing\ntokyo\nn";
+        System.setIn(new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8)));
+        server.createQuiz();
+        System.setIn(null);
+        assertEquals("test quiz", server.getQuizList().get(0).getQuizName());
     }
 
 }
