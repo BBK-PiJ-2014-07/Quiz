51f2080758d841c4ed251566eb57c6716dff5c03
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-02 11:44:51 +0000

    Wrote test to check that answering question correctly works

--- src/tests/TestQuiz.java
+++ src/tests/TestQuiz.java
@@ -5,18 +5,22 @@ import static org.junit.Assert.*;
 
 import quiz.Quiz;
 
+
+
 /**
  * Created by Sophie on 24/01/15.
  */
 public class TestQuiz {
+    private Quiz quiz;
+    @Before
+    public void buildUp(){
+        quiz = new Quiz();
+    }
 
     @Test
-    public void testQuestionAddAnswers(){
-        Quiz.Question testQ = new Quiz.Question(1, "What is the capital of France?");
-        testQ.addAnswer("Paris");
-        testQ.addAnswer("Moscow");
-        testQ.addAnswer("London");
-        testQ.addAnswer("Berlin");
-        assertEquals(testQ.getAnswerList.get(3),"Paris");
+    public void testQuestion(){
+        quiz.addQuestion("what is the capital of France", "paris","london","rome","brussels");
+        assertTrue(quiz.answerQuestion("paris"));
     }
+
 }
