7389fbcb59acbd29ad7be25c093a31b05ef32447
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-02 14:07:23 +0000

    Instantiate internal ArrayList of quizzes, and create getter

--- src/quiz/QuizServer.java
+++ src/quiz/QuizServer.java
@@ -1,9 +1,17 @@
 package quiz;
 
+import java.util.ArrayList;
+
 /**
  * Created by Sophie on 02/02/15.
  */
 public class QuizServer implements QuizService {
+    private ArrayList<Quiz> quizList;
+
+    public QuizServer(){
+        quizList = new ArrayList<>();
+    }
+
     @Override
     public int playQuiz(int quizId) {
         return 0;
@@ -17,5 +25,13 @@ public class QuizServer implements QuizService {
     @Override
     public void closeQuiz(int quizId) {
 
+    }
+
+    /**
+     * Get the internal quiz list
+     * @return the list of quizzes
+     */
+    public ArrayList<Quiz> getQuizList(){
+        return quizList;
     }
 }
