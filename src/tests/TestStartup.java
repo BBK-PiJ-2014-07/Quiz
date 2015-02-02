80d18a8893205d5974a2e0e6e88450b99dc019bd
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-02 12:01:45 +0000

    Create test class for start-up client

---
+++ src/tests/TestStartup.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import quiz.*;
+/**
+ * Created by Sophie on 02/02/15.
+ * Test the start-up client
+ */
+public class TestStartup {
+}
