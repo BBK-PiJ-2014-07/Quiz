147c5654cd5747fa5b6dc0573c35394b9112c09f
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-02 15:17:08 +0000

    Move main functionality of createQuiz() to SetupClient as this is the user-facing program. Instantiate Server and Registry in constructor of SetupClient

--- src/quiz/SetupClient.java
+++ src/quiz/SetupClient.java
@@ -1,7 +1,67 @@
 package quiz;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Scanner;
+
 /**
- * Created by Sophie on 02/02/15.
+ * The client program for creating and closing quizzes.
  */
 public class SetupClient {
+    QuizServer server;
+
+    public SetupClient(){
+        try {
+            Remote service = Naming.lookup("//localhost/");
+            server = (QuizServer) service;
+
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createQuiz(){
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.println("Please enter a name for the quiz.");
+        String quizName = inputScanner.nextLine();
+        //TODO - check to see if quiz name exists already
+        int newQuizId = server.createQuiz(quizName);
+        Boolean finished = false;
+
+        while (!finished) {
+            //TODO to be simplified
+            System.out.println("Please enter a question.");
+            String newQuestion = inputScanner.nextLine();
+            System.out.println("Please enter the CORRECT ANSWER for the question \"" + newQuestion+"\"");
+            String correctAnswer = inputScanner.nextLine();
+            System.out.println("Please enter 3 incorrect answers.");
+            System.out.print("1: ");
+            String wrong1 = inputScanner.nextLine();
+            System.out.print("\n2: ");
+            String wrong2 = inputScanner.nextLine();
+            System.out.print("\n3: ");
+            String wrong3 = inputScanner.nextLine();
+            server.addQuestion(newQuizId, newQuestion, correctAnswer,wrong1, wrong2, wrong 3);
+            System.out.println("Do you want to add another question? Y/N");
+            String response = inputScanner.nextLine();
+
+            while (!response.equals("y") && !response.equals("Y") && !response.equals("n") & !response.equals("N")){
+                System.out.println("Please enter Y or N.");
+                response = inputScanner.nextLine();
+            }
+            if (response.equals("n") || response.equals("N")) {
+                finished = true;
+            }
+        }
+        inputScanner.close();
+
+        System.out.print("Quiz " + server.createQuiz() + " created!");
+    }
 }
