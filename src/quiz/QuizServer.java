00c0fdbd8ed79b11a091af431daacec2d2b0a9ec
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-02 13:24:02 +0000

    Create class QuizServer, default implementations of interface methods

---
+++ src/quiz/QuizServer.java
@@ -0,0 +1,21 @@
+package quiz;
+
+/**
+ * Created by Sophie on 02/02/15.
+ */
+public class QuizServer implements QuizService {
+    @Override
+    public int playQuiz(int quizId) {
+        return 0;
+    }
+
+    @Override
+    public int createQuiz() {
+        return 0;
+    }
+
+    @Override
+    public void closeQuiz(int quizId) {
+
+    }
+}
