e6cbc4a13e18a4891715274d29605d96f5c1e143
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-02 14:05:27 +0000

    Write testCreateQuiz, set system.in to byte array

---
+++ src/tests/TestServer.java
@@ -0,0 +1,27 @@
+package tests;
+
+import quiz.QuizServer;
+import org.junit.*;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.*;
+
+public class TestServer {
+    private QuizServer server;
+
+    @Before
+    public void buildUp(){
+        server = new QuizServer();
+    }
+
+    @Test
+    public void testCreateQuiz(){
+        String input = "test quiz\nwhat is the capital of england?\nlondon\nparis\nbeijing\ntokyo\nn";
+        System.setIn(new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8)));
+        server.createQuiz();
+        System.setIn(null);
+        assertEquals("test quiz", server.getQuizList().get(0).getQuizName);
+    }
+}
