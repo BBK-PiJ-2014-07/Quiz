e64890badd2e95709dfdf7db205d8619ff9e0d16
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-10 17:47:22 +0000

    Write RMI bits for PlayerClient

--- src/quiz/PlayerClient.java
+++ src/quiz/PlayerClient.java
@@ -1,8 +1,26 @@
 package quiz;
 
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
 /**
  * Client for playing quiz games.
  * @author Sophie Koonin
  */
 public class PlayerClient {
+    public static void main(String[] args) {
+        try {
+            Remote service = Naming.lookup("//127.0.0.1/QuizService");
+            QuizService quizServer = (QuizServer) service;
+            //TODO
+
+        } catch (RemoteException | MalformedURLException | NotBoundException ex) {
+            ex.printStackTrace();
+
+        }
+    }
 }
