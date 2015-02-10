62d7f3d531ee5cf9cbfb34171957c2023e3add13
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-10 17:43:14 +0000

    Add Mockito imports, build up and mocking of server, then testPlayQuiz

--- src/tests/TestPlayerClient.java
+++ src/tests/TestPlayerClient.java
@@ -1,10 +1,41 @@
 package tests;
 import org.junit.*;
-import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import quiz.PlayerClient;
+import quiz.QuizServer;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Testing class for Player Client
  * @see quiz.PlayerClient
  */
+@RunWith(MockitoJUnitRunner.class)
 public class TestPlayerClient {
+    private PlayerClient player;
+
+    @Mock
+    QuizServer server;
+
+    @BeforeClass
+    public void buildUp(){
+        player = new PlayerClient();
+        server.createQuiz("test quiz");
+        server.addQuestion(1, "What comes after A?","B","C","D","E");
+        server.addQuestion(1, "What is 1+1?","2","3","4","5");
+    }
+
+    @Test
+    public void testPlayQuiz(){
+        String input = "B\n2";
+        System.setIn(new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8)));
+        player.playQuiz(1);
+
+    }
 }
