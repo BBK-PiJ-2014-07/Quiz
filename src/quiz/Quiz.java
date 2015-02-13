c2f06fdfbea7162b351e7e01f76fdfc2e7dd3147
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-13 11:35:43 +0000

    Refactor: rename closeQuiz -> setClosed(); add comments and line breaks for clarity

--- src/quiz/Quiz.java
+++ src/quiz/Quiz.java
@@ -30,36 +30,41 @@ public class Quiz implements Serializable {
     }
 
     /**
-     * Add questions to the quiz.
+     * Add questions to the quiz. Loops until user is finished
      */
     public void addQuestions(){
         boolean finished = false;
         String question;
-        String[] answers = new String[4];
+        String[] answers = new String[4];   //Each question should only have 4 answers
         Scanner input = new Scanner(System.in);
+
         while (!finished){
             System.out.print("\nPlease enter a question: ");
-            question = input.nextLine();
+            question = input.nextLine();    //get question from user
             System.out.print("\nPlease enter the CORRECT answer: ");
-            answers[0] = input.nextLine();
+            answers[0] = input.nextLine();  //first member of answer array is correct answer
+
             for (int i=1; i<4; i++){
                 System.out.print("\nPlease enter an incorrect answer: ");
-                answers[i] = input.nextLine();
+                answers[i] = input.nextLine();  //Populate rest of answer array with incorrect answers
             }
-            Question newQ = new Question(questions.size()+1, question);
-            newQ.addAnswers(answers);
-            questions.add(newQ);
-            boolean confirm = false;
+
+            Question newQ = new Question(questions.size()+1, question); //instantiate new question wih this data
+            newQ.addAnswers(answers);   //add the answers
+            questions.add(newQ);        //add this question to the internal array of questions
+
+            boolean confirm = false;    // Ask whether user is finished
             while (!confirm) {
                 System.out.print("\nDo you want to add another question? Y/N: ");
                 String ans = input.nextLine();
+
                 if (ans.toUpperCase().equals("N")) {
-                    finished = true;
+                    finished = true;    //User finished; stop the loop
                     confirm = true;
                 } else if (ans.toUpperCase().equals("Y")) {
                     confirm = true;
                 }
-                else {
+                else {  //If user enters neither Y or N it will loop again
                     System.out.println("Please enter Y or N!");
                 }
             }
@@ -68,7 +73,7 @@ public class Quiz implements Serializable {
     }
 
     /**
-     * Answer a question
+     * Answer a question (check against question's internal correctAnswer)
      * @return boolean - whether the answer is correct
      */
     public boolean answerQuestion(int questionNo, String answer){
@@ -78,7 +83,7 @@ public class Quiz implements Serializable {
     /**
      * Close the quiz.
      */
-    public void closeQuiz() {closed = true;}
+    public void setClosed() {closed = true;}
 
     /**
      * Check whether the quiz is closed.
@@ -107,7 +112,7 @@ public class Quiz implements Serializable {
             for (String answer: ans){
                 answers.add(answer);
             }
-            correctAnswer = answers.get(0);
+            correctAnswer = answers.get(0); //assign correct answer
             //randomise order of answers
             Collections.shuffle(answers);
         }
