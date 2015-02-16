17dd54235773d164f88d63bea7e6742be4aff82b
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-16 11:34:16 +0000

    static playerIds 0 -> 1 to avoid player id 0

--- src/resource/Player.java
+++ src/resource/Player.java
@@ -11,7 +11,7 @@ import java.util.TreeMap;
 public class Player {
     private String name;
     private int id;
-    private static int playerIds = 0;
+    private static int playerIds = 1;
     private Map<Integer,Integer> scores;
 
     public Player(String name){
