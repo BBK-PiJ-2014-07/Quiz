a123a8c53c06d15bb469155dce14eea7018188aa
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-16 10:55:41 +0000

    Create class Client as superclass of Player and Setup clients. Move connectServer() to Client. Remove connectServer() from PlayerClient

---
+++ src/clients/Client.java
@@ -0,0 +1,33 @@
+package clients;
+
+import service.QuizService;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+/**
+ * Superclass for client classes
+ */
+public class Client {
+    private QuizService server;
+
+
+    /**
+     * Connect to the server
+     * @return response string (TODO - this is for testing only)
+     */
+    public String connectServer(){
+
+        try {
+            Registry registry = LocateRegistry.getRegistry(1099); //TODO
+            server = (QuizService) registry.lookup("QuizService");
+            return server.sendResponse();
+        } catch (RemoteException | NotBoundException ex) {
+            ex.printStackTrace();
+        }
+        return "no response";
+    }
+
+}
--- src/clients/PlayerClient.java
+++ src/clients/PlayerClient.java
@@ -1,39 +1,12 @@
 package clients;
 
-
-import server.QuizServer;
-import service.QuizService;
-
-import java.rmi.NotBoundException;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-
 /**
  * Client for playing quiz games.
  * @author Sophie Koonin
  */
-public class PlayerClient {
-    private QuizService server;
+public class PlayerClient extends Client {
 
-    public PlayerClient(){
-    }
-    /**
-     * Connect to the server
-     * @return response string (TODO - this is for testing only)
-     */
-    public String connectServer(){
-
-        try {
-            Registry registry = LocateRegistry.getRegistry(1099); //TODO
-            server = (QuizService) registry.lookup("QuizService");
-            return server.sendResponse();
-        } catch (RemoteException | NotBoundException ex) {
-            ex.printStackTrace();
-        }
-        return "no response";
-    }
+    public PlayerClient(){ super(); }
 
     public static void main(String[] args) {
         PlayerClient pc = new PlayerClient();
