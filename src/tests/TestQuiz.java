08c87c51ed54dcf5fca5d62171ca64db26c06f75
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-17 19:11:07 +0000

    Refactor

--- src/tests/TestQuiz.java
+++ src/tests/TestQuiz.java
@@ -3,10 +3,10 @@ package tests;
 import org.junit.*;
 import static org.junit.Assert.*;
 
+import resource.Question;
 import resource.Quiz;
-
-import java.io.ByteArrayInputStream;
-import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -19,9 +19,11 @@ public class TestQuiz {
     @Before
     public void buildUp(){
         quiz = new Quiz("test quiz");
-        String input = "what is the capital of France\nparis\nlondon\nrome\nbrussels\nN";
-        System.setIn(new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8)));
-        quiz.addQuestions();
+        Question q1 = new Question(1, "What is the capital of France?");
+        q1.addAnswers("paris", "brussels", "london", "tokyo");
+        List<Question> qList = new ArrayList<>();
+        qList.add(q1);
+        quiz.addQuestions(qList);
     }
 
     @Test
