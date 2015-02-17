fefcc42deb494c67978bbd3ca49edc2d18ad71bf
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-17 18:49:44 +0000

    Move functionality of addQuestions to Setup/Server (in progress). addQuestions takes array of questions as parameter, adds each one to internal list of questions

--- src/resource/Quiz.java
+++ src/resource/Quiz.java
@@ -32,48 +32,13 @@ public class Quiz implements Serializable {
     }
 
     /**
-     * Add questions to the quiz. Loops until user is finished
+     * Add questions to the quiz.
      */
-    public void addQuestions(){
-        boolean finished = false;
-        String question;
-        String[] answers = new String[4];   //Each question should only have 4 answers
-        Scanner input = new Scanner(System.in);
-
-        while (!finished){
-            System.out.print("\nPlease enter a question: ");
-            question = input.nextLine();    //get question from user
-            System.out.print("\nPlease enter the CORRECT answer: ");
-            answers[0] = input.nextLine();  //first member of answer array is correct answer
-
-            for (int i=1; i<4; i++){
-                System.out.print("\nPlease enter an incorrect answer: ");
-                answers[i] = input.nextLine();  //Populate rest of answer array with incorrect answers
-            }
-
-            Question newQ = new Question(questions.size()+1, question); //instantiate new question wih this data
-            newQ.addAnswers(answers);   //add the answers
-            questions.add(newQ);        //add this question to the internal array of questions
-
-            boolean confirm = false;    // Ask whether user is finished
-            while (!confirm) {
-                System.out.print("\nDo you want to add another question? Y/N: ");
-                String ans = input.nextLine();
-
-                if (ans.toUpperCase().equals("N")) {
-                    finished = true;    //User finished; stop the loop
-                    confirm = true;
-                } else if (ans.toUpperCase().equals("Y")) {
-                    confirm = true;
-                }
-                else {  //If user enters neither Y or N it will loop again
-                    System.out.println("Please enter Y or N!");
-                }
-            }
-
+    public void addQuestions(Question...newQs) {
+        for (Question q: newQs){
+            questions.add(q);
         }
     }
-
     /**
      * Answer a question (check against question's internal correctAnswer)
      * @return boolean - whether the answer is correct
