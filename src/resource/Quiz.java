e7577512410cb4834013599d5a2d70bd8a921d86
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-17 19:20:13 +0000

    Remove unused import

--- src/resource/Quiz.java
+++ src/resource/Quiz.java
@@ -6,7 +6,6 @@ import java.io.Serializable;
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * The quiz itself. Contains inner class Question.
