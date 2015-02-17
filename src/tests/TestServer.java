475a1fa244ec77ca561931b66c902ac5bc40650a
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-17 15:58:23 +0000

    Rollback changes

--- src/tests/TestServer.java
+++ src/tests/TestServer.java
@@ -38,6 +38,37 @@ public class TestServer {
         assertTrue(server.getQuizList().get(0).isClosed());
     }
 
+    @Test   //quiz 4
+    public void testPlayQuizScore(){
+        String answers="paris\n2";
+        System.setIn(new ByteArrayInputStream(answers.getBytes(StandardCharsets.UTF_8)));
+        int score = server.playQuiz(4,player1);
+        assertEquals(2, score);
+    }
 
+    @Test   //quiz 5
+    public void testPlayQuizWrongId(){
+        assertEquals(-1,server.playQuiz(999,player1));
+    }
+
+    @Test   //quiz 6
+    public void testPlayQuizWrongAns(){
+        String answers="paris\n5";
+        System.setIn(new ByteArrayInputStream(answers.getBytes(StandardCharsets.UTF_8)));
+        int score = server.playQuiz(6,player1);
+        assertEquals(1, score);
+    }
+
+    @Test   //quiz 7
+    public void testHighScore(){
+        Player player2 = new Player("Lindsay");
+        String answers="paris\n5";
+        System.setIn(new ByteArrayInputStream(answers.getBytes(StandardCharsets.UTF_8)));
+        server.playQuiz(7,player1);
+        String newAnswers = "paris\n2";
+        System.setIn(new ByteArrayInputStream(newAnswers.getBytes(StandardCharsets.UTF_8)));
+        server.playQuiz(7,player2);
+        assertEquals(server.getQuizList().get(0).getHighScore().getKey().getName(),"Lindsay");
+    }
 }
 
