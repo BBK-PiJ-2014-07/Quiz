ca854c282ab8d33988397cb74ff733036458b23a
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-21 10:09:04 +0000

    Implement playQuiz

--- src/clients/PlayerClient.java
+++ src/clients/PlayerClient.java
@@ -1,12 +1,15 @@
 package clients;
 
-import resource.Player;
+import resource.Question;
+import resource.Quiz;
 import service.QuizService;
 
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Client for playing quiz games.
@@ -33,9 +36,26 @@ public class PlayerClient {
         //TODO get high scores
     }
 
-    public void playQuiz(int quizId, Player player) throws RemoteException {
-        server.playQuiz(quizId,player);
-        //TODO
+    public int playQuiz(int quizId, int playerId) throws RemoteException {
+        // print the question
+        // print the answers
+        // user answers -> call to server answerQuestion()
+        // server returns score at end
+
+        //check the server list of quizzes and players to ensure that id exists
+        if (server.getQuizList().stream().noneMatch(q -> q.getId() == quizId) || server.getPlayerList().stream().noneMatch(p -> p.getId() == playerId)) {
+            return -1;
+        }
+        List<String> answers = new ArrayList<>();
+        Quiz thisQuiz = server.getQuizList().stream().filter(q -> q.getId() == quizId).findFirst().get();   //get the quiz
+
+        for (Question q: thisQuiz.getQuestions()){
+            System.out.println(q); //print the question
+            System.out.print("\nEnter your answer: ");
+            String ans = System.console().readLine();   //get the answer from the user
+            answers.add(ans);   //add the answer to the list
+        }
+        return server.playQuiz(quizId, playerId, answers);  //play the quiz to get the score
 
     }
 }
