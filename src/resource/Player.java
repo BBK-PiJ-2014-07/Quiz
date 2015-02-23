fe65b12409cb29813d76f21631880b068192645a
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-23 11:00:00 +0000

    Remove Player score map

--- src/resource/Player.java
+++ src/resource/Player.java
@@ -14,32 +14,11 @@ public class Player implements Serializable {
     private String name;
     private int id;
     private static int playerIds = 1;
-    private Map<Integer,Integer> scores;
-
     public Player(String name){
         this.name = name;
-        scores = new TreeMap<>();
         id = playerIds++;
     }
 
-    /**
-     * Add a score to the internal map
-     * @param quizId - the id of the quiz played
-     * @param newScore - the score
-     */
-    public int addScore(int quizId, int newScore){
-        //check to see if the quiz already has a score associated with it
-        // if the existing score is HIGHER just leave the existing high score for that quiz
-        if (scores.containsKey(quizId) && (scores.get(quizId) > newScore)) {
-                return scores.get(quizId);
-        }
-        scores.put(quizId,newScore);
-        return newScore;
-    }
-
-    public int getHighScore(){
-        return scores.values().stream().max((x,y) -> x > y ? 1:-1).get();
-    }
 
     /**
      * Override equals to allow for easy comparison
