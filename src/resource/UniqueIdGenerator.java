09e2c76c2d5881361ed5eeb3afd26f42804ea906
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-24 20:29:46 +0000

    Initialise UniqueIdGenerator to 1 to prevent id = 0

--- src/resource/UniqueIdGenerator.java
+++ src/resource/UniqueIdGenerator.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
  * Singleton class to allow for non-repetition of quiz id.
  */
 public class UniqueIdGenerator implements Serializable {
-    private int nextId = 0;
+    private int nextId = 1;
 
     public UniqueIdGenerator(){}
 
