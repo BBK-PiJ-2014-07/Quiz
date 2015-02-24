953a22d4980b02244f7243f1d9ea9dd222d27ea8
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-24 19:18:17 +0000

    Make serializable

--- src/resource/QuizId.java
+++ src/resource/QuizId.java
@@ -1,9 +1,11 @@
 package resource;
 
+import java.io.Serializable;
+
 /**
  * Singleton class to allow for non-repetition of quiz id.
  */
-public class QuizId {
+public class QuizId implements Serializable {
     private static int nextId = 0;
 
     public QuizId(){}
